6566d0731f9b1667b4be3bc0744fe29ace1f9091
Justen Schilinski <justenschilinski34@hotmail.com>
2023-02-23 22:31:28 -0500

    Added fontawesome arrows to break and session time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
@@ -15,19 +16,22 @@ function App() {
         </Row>
          <Row>
           <Col>
-            <button></button>
+            <i><FaArrowDown /></i>
             time
-            <button></button>
+            <i><FaArrowUp /></i>
           </Col>
           <Col>
-            <button></button>
+            <i><FaArrowDown /></i>
             time
-            <button></button>
+            <i><FaArrowUp /></i>
           </Col>
         </Row>
         <Row>
           <Col md={{ span: 6, offset: 3 }}>
-          <Card bg={'secondary'} style={{ width: 'calc(30rem + 1vmin)', height: 'calc(16rem + 2vmin)'}}>This will be time display</Card>
+            <Card bg={'secondary'} style={{ width: 'calc(30rem + 1vmin)', height: 'calc(16rem + 2vmin)'}}>
+              <Row>Session</Row>
+              <Row>Time Display</Row>
+            </Card>
           </Col>
         </Row>
       </Container>
