a7f1098dfacd405bb78fc33a2da684791e87f577
Justen Schilinski <justenschilinski34@hotmail.com>
2023-02-25 17:10:14 -0500

    Added play and reset buttons, customized layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import { HiPlayPause } from "react-icons/hi2";
+import { MdOutlineRestartAlt } from "react-icons/md";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
@@ -10,6 +12,9 @@ function App() {
   return (
     <div className="App">
       <Container>
+        <Row className="mx-auto">
+          <h1>25 + 5 Clock</h1>
+        </Row>
         <Row>
           <Col>Break Length</Col>
           <Col>Session Length</Col>
@@ -27,11 +32,19 @@ function App() {
           </Col>
         </Row>
         <Row>
-          <Col md={{ span: 6, offset: 3 }}>
-            <Card bg={'secondary'} style={{ width: 'calc(30rem + 1vmin)', height: 'calc(16rem + 2vmin)'}}>
-              <Row>Session</Row>
-              <Row>Time Display</Row>
+          <Col className="d-flex justify-content-center">
+            <Card bg={'secondary'} style={{ width: 'calc(25rem + 1vmin)', height: 'calc(12rem + 2vmin)'}}>
+              <Row className="mx-auto">Session</Row>
+              <Row className="mx-auto">Time Display</Row>
             </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="d-flex justify-content-end">
+            <i id="start_stop"><HiPlayPause /></i>
+          </Col>
+          <Col className="d-flex justify-content-start">
+            <i id="reset"><MdOutlineRestartAlt /></i>
           </Col>
         </Row>
       </Container>
